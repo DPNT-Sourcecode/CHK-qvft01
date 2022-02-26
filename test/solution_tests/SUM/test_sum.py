a3a585e4853c62a1bb92554bb70d4e902f30a5ec
sbx_user1051 <sbx_user1051@169.254.84.73>
2022-02-26 13:26:30 +0000

    Sat Feb 26 13:26:30 UTC 2022

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -9,15 +9,15 @@ class TestSum():
         assert sum_solution.compute(1, 2) == 3
 
     def test_sum_negative_values(self):
-        assert sum_solution.compute(-10, 10) == 3
+        with pytest.raises(ValidationException):
+            sum_solution.compute(-10, 10) == 3
 
-    def test_sum_validates_arguments_greater_than_100(self):
-        expected_exception = Exception("Cannot pass values greater than 100")
-        actual_exception = None
+    def test_sum_validates_arguments_greater_than_100(self, caplog):
         with pytest.raises(ValidationException) as context:
-            actual_exception = sum_solution.compute(200, 300)
+            sum_solution.compute(200, 300)
 
-        assert expected_exception == actual_exception
+        breakpoint()
+
 
 
 
