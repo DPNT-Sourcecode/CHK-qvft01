7b5f45988caeb3c1b4a46086672ed893a306ad76
sbx_user1051 <sbx_user1051@169.254.84.73>
2022-02-26 13:47:18 +0000

    Sat Feb 26 13:47:18 UTC 2022

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -1,15 +1,11 @@
-from lib.solutions.HLO.hello_solution import hello
 from solutions.CHK import checkout_solution
 
-def test_hello_message_prints_correctly():
-    assert hello_solution.hello() == "Hello, World!"
+def test_add_item_to_checkout():
+    item = 'A'
+    checkout_solution.checkout(item)
 
-def test_hello_message_without_name():
-    assert hello_solution.hello() == "Hello, World!"
+def test_attempt_to_add_non_existant_item():
+    item = 1
+    checkout_solution.checkout(item)
 
-def test_hello_with_friend_name():
-    friend_name = "Alex"
-    assert hello_solution.hello(friend_name) == 'Hello, Alex!'
-
-    friend_name = "John"
-    assert hello_solution.hello(friend_name) == 'Hello, John!'
+def test_remove_item_added_to_cart()
