89924bef293f4cb8cf2484d7c0f41ae7e48d179e
sbx_user1051 <sbx_user1051@169.254.84.73>
2022-02-26 14:08:18 +0000

    Sat Feb 26 14:08:18 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -11,7 +11,11 @@ STOCK_LIST_BY_SKUS = (
     )
 
 STOCK_LIST = [
-    {'sku': 'A', 'price': 50, }
+    {'sku': 'A', 'price': 50, 'offer': '3A for 130'},
+    {'sku': 'B', 'price': 30, 'offer': '3A for 130'},
+    {'sku': 'C', 'price': 20, 'offer': None},
+    {'sku': 'D', 'price': 15, 'offer': None},
+
 ]
     
 
@@ -19,13 +23,19 @@ def checkout(skus):
     price = 0
     skus_to_list = skus.split(',')
 
+    breakpoint()
+
+    for sku in skus_to_list:
+        for stock_item in STOCK_LIST:
+            
+
     for sku in skus_to_list:
         for stock_item in STOCK_LIST_BY_SKUS:
             label = stock_item[0]
             if label == sku:
                 price += stock_item[1]
-            breakpoint()
     raise NotImplementedError()
+
 
 
 
