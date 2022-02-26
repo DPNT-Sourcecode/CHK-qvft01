1bb1347b153c4554f397799fb6e31225fd27f9de
sbx_user1051 <sbx_user1051@169.254.84.73>
2022-02-26 13:29:30 +0000

    Sat Feb 26 13:29:30 UTC 2022

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -12,11 +12,10 @@ class TestSum():
         with pytest.raises(ValidationException):
             sum_solution.compute(-10, 10) == 3
 
-    def test_sum_validates_arguments_greater_than_100(self, caplog):
-        with pytest.raises(ValidationException) as context:
+    def test_sum_validates_arguments_greater_than_100(self):
+        with pytest.raises(ValidationException):
             sum_solution.compute(200, 300)
 
-        breakpoint()
 
 
 
