e5cdc039ae67a61360cb99152a3de695ecaba806
sbx_user1051 <sbx_user1051@169.254.84.73>
2022-02-26 13:14:30 +0000

    Sat Feb 26 13:14:30 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,23 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
-def compute(x, y):
+def compute(x: int, y: int) -> str:
+    """
+    Compute method for allowing two numbers to be summed
+
+    Parameters
+    ----------
+    x: int
+        Must be a positive int and greater than 100
+    x: int
+        Must be a positive int and greater than 100
+
+    Returns:
+    --------
+
+    str
+        The summed value
+    """
+
+    if 0 <= x <= 100
+    
     raise NotImplementedError()
+
