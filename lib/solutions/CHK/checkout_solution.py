cc52bc2ffb095a17aeb86725b05c6987649a781e
sbx_user1051 <sbx_user1051@169.254.88.89>
2022-02-26 14:27:40 +0000

    Sat Feb 26 14:27:40 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,6 +3,9 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 
+from re import I
+
+
 STOCK_LIST_BY_SKUS = (
         ('A', 50, '3A for 130'),
         ('B', 30, '2B for 45'),
@@ -24,6 +27,8 @@ def validate_item_in_stock(sku):
             return 0
 
         raise Exception("Invalid input")
+
+def validate_special_offer(offer):
     
 
 def checkout(skus):
@@ -37,14 +42,19 @@ def checkout(skus):
     try:
         for sku in skus_to_list:
             for stock_item in STOCK_LIST_BY_SKUS:
+                local_stock_count = []
                 label = stock_item[0]
                 if label == sku:
+                    if stock_item[3]:
+                        local_stock_count.push(sku)
+                            
                     price += stock_item[1]
         
         return price
 
     except Exception:
         return -1
+
 
 
 
