3eb05343502b5887e7254d8511640c070bd9ae7d
sbx_user1051 <sbx_user1051@169.254.84.73>
2022-02-26 13:44:18 +0000

    Sat Feb 26 13:44:18 UTC 2022

---
+++ test/solution_tests/CHK/test_chk.py
@@ -0,0 +1,15 @@
+from lib.solutions.HLO.hello_solution import hello
+from solutions.CHK import checkout_solution
+
+def test_hello_message_prints_correctly():
+    assert hello_solution.hello() == "Hello, World!"
+
+def test_hello_message_without_name():
+    assert hello_solution.hello() == "Hello, World!"
+
+def test_hello_with_friend_name():
+    friend_name = "Alex"
+    assert hello_solution.hello(friend_name) == 'Hello, Alex!'
+
+    friend_name = "John"
+    assert hello_solution.hello(friend_name) == 'Hello, John!'
