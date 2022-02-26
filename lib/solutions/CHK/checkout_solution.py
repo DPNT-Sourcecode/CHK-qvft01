8d10feb9d9074257cf26f2a7553be47287b4d3be
sbx_user1051 <sbx_user1051@169.254.156.149>
2022-02-26 21:00:38 +0000

    Sat Feb 26 21:00:38 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,59 +5,21 @@
 
 from itertools import groupby
 
-from enum import Enum
-
-class Offers(Enum):
-    BUY_3A_FOR_130 = '3A for 130',
-    BUY_2A_FOR_45 = '2B for 45',
-
-
-
-STOCK_LIST_BY_SKUS = (
-        ('A', 50, '3A for 130'),
-        ('B', 30, '2B for 45'),
-        ('C', 20, None),
-        ('D', 15, None)
-    )
-
-def get_discount_factor(discount_type: str):
-    if discount_type == Offers.BUY_3A_FOR_130.value[0]:
-        # Hard coded for now
-        return 3, 30
-    
-    if discount_type == Offers.BUY_2A_FOR_45.value[0]:
-        return 2, 15
-
-    return None, 0
-
-
-def apply_discount_factor(stock_item: STOCK_LIST_BY_SKUS, index: int):
-    discount_factor, discounted_price = get_discount_factor(stock_item[2])
-    counter = index + 1
-
-    if discount_factor:
-        if counter % discount_factor == 0:
-            return discounted_price
-        else:
-            return stock_item[1]
-    else:
-        return stock_item[1]
-
-def validate_input(skus):
-    for sku in skus:
-        for _, sku_code in enumerate(list(sku)):
-            if sku_code not in ['A', 'B', 'C', 'D']:
-                raise Exception("Invalid input detected")
-    
-    return True
-
-
-
-
-
-
-
 class ShoppingCart:
+
+    """
+    Shopping cart class for handling interactions for exercise
+
+    Arguments:
+    ---------
+
+    None
+
+    Methods
+    -------
+
+    add_item(item: str): None    
+    """
 
     def __init__(self):
         self.products = {
@@ -94,7 +56,7 @@ class ShoppingCart:
 
         self.shopping_cart = {}
 
-    def add_item(self, item):
+    def add_item(self, item: str):
         product = self.products[item]
         product['quantity'] += 1
 
@@ -104,14 +66,18 @@ class ShoppingCart:
 
     
     def _apply_discount(self, product, item):
-        get_discount_for_product = self.discount_list[item]
-        if product['quantity'] % get_discount_for_product['rule'] == 0:
-            percentage = get_discount_for_product['discount_percent']
-            
-            if percentage:
-                discount = int((product['price'] * percentage) / 100)
-                self.total += discount
+        get_discount_for_product = self.discount_list.get(item, None)
 
+        if get_discount_for_product:
+            if product['quantity'] % get_discount_for_product['rule'] == 0:
+                percentage = get_discount_for_product['discount_percent']
+                
+                if percentage:
+                    discount = int((product['price'] * percentage) / 100)
+                    self.total += discount
+
+            else:
+                self.total += product['price'] * 1
         else:
             self.total += product['price'] * 1
 
@@ -155,6 +121,7 @@ def checkout(skus: str):
     except (Exception, InvalidInputException) as e:
         breakpoint()
         return -1
+
 
 
 
