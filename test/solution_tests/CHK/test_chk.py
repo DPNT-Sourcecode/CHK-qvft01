09c68e2b662cbaf5547fa2a43f1053ae1b3a3b65
sbx_user1051 <sbx_user1051@169.254.84.73>
2022-02-26 13:50:18 +0000

    Sat Feb 26 13:50:18 UTC 2022

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -1,11 +1,19 @@
 from solutions.CHK import checkout_solution
 
 def test_add_item_to_checkout():
-    item = 'A'
-    checkout_solution.checkout(item)
+    skus = 'A'
+    checkout_solution.checkout(skus)
 
 def test_attempt_to_add_non_existant_item():
-    item = 1
-    checkout_solution.checkout(item)
+    skus = 1
+    checkout_solution.checkout(skus)
 
-def test_remove_item_added_to_cart()
+def test_add_multiple_to_checkout():
+    skus = 'A, B, C, D'
+
+    checkout_solution.checkout(skus)
+
+def test_special_offers_on_item():
+    
+    skus = 'A, B, C, D'
+
