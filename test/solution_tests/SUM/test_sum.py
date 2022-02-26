d58200fab8150be24cb4952a37815acd4e2cb2ef
sbx_user1051 <sbx_user1051@169.254.84.73>
2022-02-26 13:08:30 +0000

    Sat Feb 26 13:08:30 UTC 2022

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -4,3 +4,9 @@ from solutions.SUM import sum_solution
 class TestSum():
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
+
+    def test_sum_negative_values(self):
+        assert sum_solution.compute(-10, 10) == 3
+
+    def test_sum_validates_arguments_greater_than_100(self)
+
