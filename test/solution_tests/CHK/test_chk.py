01cd0e72a5deaa0ca545c84f4a4d2eb44bd343e6
sbx_user1051 <sbx_user1051@169.254.84.73>
2022-02-26 13:53:18 +0000

    Sat Feb 26 13:53:18 UTC 2022

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -1,8 +1,9 @@
+from tabnanny import check
 from solutions.CHK import checkout_solution
 
 def test_add_item_to_checkout():
     skus = 'A'
-    checkout_solution.checkout(skus)
+    assert 50 == checkout_solution.checkout(skus)
 
 def test_attempt_to_add_non_existant_item():
     skus = 1
@@ -13,7 +14,21 @@ def test_add_multiple_to_checkout():
 
     checkout_solution.checkout(skus)
 
-def test_special_offers_on_item():
+def test_special_offers_buy_2():
+    skus = 'B'
+    assert 30 == checkout_solution.checkout(skus)
     
-    skus = 'A, B, C, D'
+    skus = 'B, B'
+    assert 50 == checkout_solution.checkout(skus)
+
+def test_special_offers_buy_3():
+    skus = 'A'
+    assert 50 == checkout_solution.checkout(skus)
+    
+    skus = 'A, A'
+    assert 100 == checkout_solution.checkout(skus)
+
+    skus = 'A, A, A'
+    assert 130 == checkout_solution.checkout(skus)
+
 
