31d0017d4cc1de78fc5b23bee0d5bbe6fe33884e
sbx_user1051 <sbx_user1051@169.254.84.73>
2022-02-26 13:36:09 +0000

    Sat Feb 26 13:36:09 UTC 2022

--- test/solution_tests/HLO/test_hlo.py
+++ test/solution_tests/HLO/test_hlo.py
@@ -1,4 +1,7 @@
 from solutions.HLO import hello_solution
 
 def test_hello_message_prints_correctly():
-    assert hello_solution.hello() == ""
+    assert hello_solution.hello() == "hello to the world"
+
+def test_parameter_not_required():
+    assert hello_solution.hello('This is not used') == "hello to the world"
